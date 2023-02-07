8f0109113c9d0ce9de116c756e5e6b21c7146d4a
Man Nok Pun <sgloriasca@gmail.com>
2023-02-06 19:18:02 -0500

    Add start and end scene

--- MAPD724-ICE-4A/GameViewController.swift
+++ MAPD724-ICE-4A/GameViewController.swift
@@ -10,44 +10,29 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     @IBOutlet var LivesLabel: UILabel?
     @IBOutlet var ScoreLabel: UILabel?
+    
+    var currentScene: GKScene?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //print(self.view.frame.size.width)
-        //print(self.view.frame.size.height)
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities MDPand graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    view.ignoresSiblingOrder = true
-                }
-            }
-        }
+        setScene(sceneName: "EndScene")
         
         CollisionManager.gameViewController = self
         ScoreManager.Score = 0
         ScoreManager.Lives = 5
         updateLivesLabel()
         updateScoreLabel()
-
+        
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .portrait
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -58,5 +43,25 @@ class GameViewController: UIViewController {
     
     func updateLivesLabel(){
         LivesLabel?.text = "Lives: \(ScoreManager.Lives)"
+    }
+    
+    func setScene( sceneName: String ) {
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities MDPand graphs.
+        currentScene = GKScene(fileNamed: sceneName)
+            
+            // Get the SKScene from the loaded GKScene
+        if let sceneNode = currentScene!.rootNode as! SKScene? {
+            // Set the scale mode to scale to fit the window
+            sceneNode.scaleMode = .aspectFill
+            
+            // Present the scene
+            if let view = self.view as! SKView? {
+                view.presentScene(sceneNode)
+                view.ignoresSiblingOrder = true
+            }
+        }
+        
     }
 }
---
+++ MAPD724-ICE-4A/Scenes/EndScene.swift
@@ -0,0 +1,67 @@
+//
+//  StartScene.swift
+//  MAPD724-ICE-4A
+//
+//  Created by Man Nok Pun on 2023-02-06.
+//
+
+import UIKit
+import AVFoundation
+import SpriteKit
+import GameplayKit
+
+class EndScene: SKScene {
+    
+    // instance variables
+    var ocean1: Ocean?
+    var ocean2: Ocean?
+    
+    var screenSize = UIScreen.main.bounds
+    var screenHeight: CGFloat?
+    var screenWidth: CGFloat?
+    
+    override func sceneDidLoad() {
+      name = "END"
+        //add ocean to scene
+        ocean1 = Ocean()  // instantiate new Ocean allocate memory
+        ocean1?.Reset()
+        addChild(ocean1!)
+        
+        ocean2 = Ocean()  // instantiate new Ocean allocate memory
+        ocean2?.position.y = -627
+        addChild(ocean2!)
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    // triggered every frame (about 60 frames/sec
+    override func update(_ currentTime: TimeInterval) {
+        ocean1?.Update()
+        ocean2?.Update()
+    }
+}
